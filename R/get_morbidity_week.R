57ae501d4114a3143c45b110e86f5b3de0cabed7
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-02-07 17:05:53 +0800

    Added get_morbidity_week function. Added license.

---
+++ R/get_morbidity_week.R
@@ -0,0 +1,34 @@
+utils::globalVariables(c('%>%', 'col_date', 'col_integer', 'cols', 'end', 'select', 'start', 'to_get', 'week', 'year'))
+
+#' Gets morbidity week of a given date.
+#'
+#' @param date the date which we get the morbidity week for
+#'
+#' @returns Integer of morbidity week
+#' @export
+#'
+get_morbidity_week <- function(date) {
+  from_date <- as.Date(date)
+
+  # reads morbidity_weeks.csv, which contains all dates for morbidity weeks
+  morbidity_weeks <- system.file('extdata', 'morbidity_weeks.csv', package='tbandothers') %>%
+    readr::read_csv(
+      show_col_types=FALSE,
+      col_types = cols(
+         year = col_integer(),
+         week = col_integer(),
+         start = col_date(format='%m/%d/%Y'),
+         end = col_date(format='%m/%d/%Y')
+      )
+    )
+
+  morbidity_weeks$to_get <- from_date
+
+  result <- morbidity_weeks %>%
+    dplyr::filter(year == lubridate::year(from_date)) %>%
+    dplyr::filter(start <= to_get & to_get <= end) %>%
+    select(week)
+
+  if (nrow(result) > 0) return(result$week)
+  else return(NA)
+}
