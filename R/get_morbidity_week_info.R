dfe0f06a26cf30aa930e47bcc535b0f3a83338c3
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-02-13 14:36:44 +0800

    Allowed get_morbidity_week_info to not require from_year. If a year is not given, then the current year will be used.

--- R/get_morbidity_week_info.R
+++ R/get_morbidity_week_info.R
@@ -3,11 +3,13 @@
 #' Accepts a week number and year, then returns info about the morbidity week. This info contains the year, week number, start date, and end date.
 #'
 #' @param week_number Week number of desired morbidity week.
-#' @param from_year Year for what morbidity week calendar to use.
+#' @param from_year Optional. Year for what morbidity week calendar to use. If not given, then the current year is used.
+#'
+#' @import lubridate
 #'
 #' @returns A tibble. This can be then used to call for specific morbidity week info using dplyr::pull()
 #' @export
-get_morbidity_week_info <- function(week_number, from_year) {
+get_morbidity_week_info <- function(week_number, from_year=NULL) {
   # authorize and read google sheet
   if (!googlesheets4::gs4_has_token()) {
     googlesheets4::gs4_auth(scopes='https://www.googleapis.com/auth/spreadsheets.readonly')
@@ -22,6 +24,12 @@ get_morbidity_week_info <- function(week_number, from_year) {
     end = as.Date(end)
   )
 
+  # if from_year is not given, then the current year is used
+  if (is.null(from_year)) {
+    from_year <- lubridate::year(Sys.Date())
+  }
+
+  # filter and return morbidity week dataframe using arguments
   morbidity_week_info <- morbidity_weeks %>% filter(year==from_year, week==week_number)
 
   return(head(morbidity_week_info, 1))
