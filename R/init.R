a0525ec21e396b4e07b15dc2124a164ddf1548ed
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-02-20 13:34:22 +0800

    Changed init to use .onLoad() for authentication services.

--- R/init.R
+++ R/init.R
@@ -1,18 +1,20 @@
-# initialize BigQuery connection; requests authorization key input
-bigrquery::bq_auth()
+.onLoad <- function(libname, pkgname) {
+  # initialize BigQuery connection; requests authorization key input
+  bigrquery::bq_auth()
 
-# initialize Google Drive connection
-googledrive::drive_auth()
+  # initialize Google Drive connection
+  googledrive::drive_auth()
 
-# initialize Google Sheets connection
-googlesheets4::gs4_auth()
+  # initialize Google Sheets connection
+  googlesheets4::gs4_auth()
 
-# read morbidity week Google Sheet
-morbidity_weeks <- googlesheets4::read_sheet('1lTwxbm96nTffaM0WG77lhpNcqDBlLDQJ3CfQFrWuAmE', sheet='morbidity_weeks')
+  # read morbidity week Google Sheet
+  morbidity_weeks <- googlesheets4::read_sheet('1lTwxbm96nTffaM0WG77lhpNcqDBlLDQJ3CfQFrWuAmE', sheet='morbidity_weeks')
 
-morbidity_weeks <- morbidity_weeks %>% mutate(
-  year = as.numeric(year),
-  week = as.numeric(week),
-  start = as.Date(start),
-  end = as.Date(end)
-)
+  morbidity_weeks <- morbidity_weeks %>% mutate(
+    year = as.numeric(year),
+    week = as.numeric(week),
+    start = as.Date(start),
+    end = as.Date(end)
+  )
+}
