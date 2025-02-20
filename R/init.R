134f3b24289982702edf97dc1772c61fcf7e41b4
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-02-20 14:26:25 +0800

    Testing morbidity_weeks access.

--- R/init.R
+++ R/init.R
@@ -7,14 +7,14 @@
 
   # initialize Google Sheets connection
   googlesheets4::gs4_auth()
-
-  # read morbidity week Google Sheet
-  morbidity_weeks <- googlesheets4::read_sheet('1lTwxbm96nTffaM0WG77lhpNcqDBlLDQJ3CfQFrWuAmE', sheet='morbidity_weeks')
-
-  morbidity_weeks <- morbidity_weeks %>% mutate(
-    year = as.numeric(year),
-    week = as.numeric(week),
-    start = as.Date(start),
-    end = as.Date(end)
-  )
 }
+
+# read morbidity week Google Sheet
+morbidity_weeks <- googlesheets4::read_sheet('1lTwxbm96nTffaM0WG77lhpNcqDBlLDQJ3CfQFrWuAmE', sheet='morbidity_weeks')
+
+morbidity_weeks <- morbidity_weeks %>% mutate(
+  year = as.numeric(year),
+  week = as.numeric(week),
+  start = as.Date(start),
+  end = as.Date(end)
+)
