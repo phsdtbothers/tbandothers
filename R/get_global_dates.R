ca85135f475b7f445a00b8a84dc1f48120dea1f4
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-02-20 13:28:51 +0800

    Fixed dateshard portion of the output. And now accounts for unique morbidity week lengths to only provide the end date.

--- R/get_global_dates.R
+++ R/get_global_dates.R
@@ -1,6 +1,6 @@
 #' Provides week information based on a given date.
 #'
-#' Provides week information based on a given date. This
+#' Provides week information based on a given date. This contains the morbidity week info for the current week and the previous week. Note that if the week does not end on a friday or a saturday, then the dateshard generated will be the final day of the week.
 #'
 #' @param run_date Optional. The date that the code will base the output on. If not given, then the current date will be used.
 #'
@@ -15,18 +15,26 @@ get_global_dates <- function(run_date=Sys.Date()) {
   this_week <- tbandothers::get_morbidity_week_number(as.Date(run_date)) %>% tbandothers::get_morbidity_week_info(from_year=lubridate::year(run_date))
 
   global_dates$current_week <- this_week$week
-  global_dates$current_dateshard <- global_dates$run_date - lubridate::days(lubridate::wday(global_dates$run_date) - 6)
   global_dates$current_start <- this_week$start
   global_dates$current_end <- this_week$end
+  global_dates$current_dateshard <- ifelse( # to account for morbidity weeks with special start dates (i.e. 2024 MW 52 does not end on a saturday)
+    lubridate::wday(this_week$end) >= 6,
+    this_week$start - lubridate::days(lubridate::wday(this_week$start) - 6),
+    this_week$end
+  ) %>% as.Date() %>% format('%Y%m%d') # format to BQ dateshard prefix
 
   # generate info for previous week
   last_week <- this_week$start - 7
-  last_week <- tbandothers::get_morbidity_week_number(last_week) %>% tbandothers::get_morbidity_week_info(from_year=lubridate::year(run_date))
+  last_week <- tbandothers::get_morbidity_week_number(last_week) %>% tbandothers::get_morbidity_week_info(from_year=lubridate::year(last_week))
 
   global_dates$previous_week <- last_week$week
-  global_dates$previous_dateshard <- global_dates$current_dateshard <- global_dates$run_date - lubridate::days(lubridate::wday(global_dates$run_date) - 6)
   global_dates$previous_start <- last_week$start
   global_dates$previous_end <- last_week$end
+  global_dates$previous_dateshard <- ifelse( # to account for morbidity weeks with special start dates (i.e. 2024 MW 52)
+    lubridate::wday(last_week$end) >= 6,
+    last_week$start - lubridate::days(lubridate::wday(last_week$start) - 6),
+    last_week$end
+  ) %>% as.Date() %>% format('%Y%m%d') # format to BQ dateshard prefix
 
   return(global_dates)
 }
