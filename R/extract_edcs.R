5bdb75e1a3099e7c777eada28f38af4e924cd5dc
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-03-20 12:31:47 +0800

    Uses run date to pull from line list instead of reporting date of morbidity week.

--- R/extract_edcs.R
+++ R/extract_edcs.R
@@ -12,15 +12,16 @@
 #' @export
 extract_edcs <- function(bq_project, bq_edcs_dataset, bq_disease, run_date=Sys.Date()) {
   date_info <- tbandothers::get_global_dates(run_date=run_date)
+  date_today <- run_date %>% format('%Y%m%d')
 
   # get columns from edcs patient and laboratory profiles
-  schema_pat <- bigrquery::bq_table_fields(bigrquery::bq_table(bq_project, bq_edcs_dataset, paste0(bq_disease, '_view_', date_info$current_dateshard))) %>% sapply(function(x) x$name)
-  schema_lab <- bigrquery::bq_table_fields(bigrquery::bq_table(bq_project, bq_edcs_dataset, paste0(bq_disease, '_view_lab_', date_info$current_dateshard))) %>% sapply(function(x) x$name)
+  schema_pat <- bigrquery::bq_table_fields(bigrquery::bq_table(bq_project, bq_edcs_dataset, paste0(bq_disease, '_view_', date_today))) %>% sapply(function(x) x$name)
+  schema_lab <- bigrquery::bq_table_fields(bigrquery::bq_table(bq_project, bq_edcs_dataset, paste0(bq_disease, '_view_lab_', date_today))) %>% sapply(function(x) x$name)
 
   schema_lab_only <- schema_lab[!schema_lab %in% schema_pat]
 
   # call from bigquery, get all pat fields, then new lab fields
-  query <- paste0("SELECT pat.*, ", paste0('lab.', schema_lab_only, collapse=','), " FROM ", bq_edcs_dataset, ".", bq_disease, "_view_", date_info$current_dateshard, " pat LEFT JOIN ", bq_edcs_dataset, ".", bq_disease, "_view_lab_", date_info$current_dateshard, " lab ON pat.case_id = lab.case_id WHERE pat.last_modified_date BETWEEN '", date_info$previous_start, "' AND '", date_info$previous_end, "' OR lab.last_modified_date BETWEEN '", date_info$previous_start, "' AND '", date_info$previous_end, "'")
+  query <- paste0("SELECT pat.*, ", paste0('lab.', schema_lab_only, collapse=','), " FROM ", bq_edcs_dataset, ".", bq_disease, "_view_", date_today, " pat LEFT JOIN ", bq_edcs_dataset, ".", bq_disease, "_view_lab_", date_today, " lab ON pat.case_id = lab.case_id WHERE pat.last_modified_date BETWEEN '", date_info$previous_start, "' AND '", date_info$previous_end, "' OR lab.last_modified_date BETWEEN '", date_info$previous_start, "' AND '", date_info$previous_end, "'")
 
   output <- bigrquery::bq_table_download(bigrquery::bq_project_query(bq_project, query))
 
