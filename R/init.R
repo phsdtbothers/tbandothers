597ac8599ecea887353411b582ff5b8b41a00c16
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-03-20 14:24:24 +0800

    fixed capitalization.

--- R/init.R
+++ R/init.R
@@ -1,4 +1,4 @@
-#' # read morbidity week Google Sheet - MUST INSTALL googlesheets4 package first
+# read morbidity week Google Sheet - MUST INSTALL AND AUTHORIZE googlesheets4 LOCALLY BEFORE LOADING
 morbidity_weeks <- googlesheets4::read_sheet('1lTwxbm96nTffaM0WG77lhpNcqDBlLDQJ3CfQFrWuAmE', sheet='morbidity_weeks')
 
 morbidity_weeks <- morbidity_weeks %>% mutate(
