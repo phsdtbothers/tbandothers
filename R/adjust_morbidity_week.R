d2c88a31ec5a7b3a8743ecb54b328bd265c7ca4e
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-03-25 14:22:08 +0800

    Also edited adjust_morbidity_week for morbidity week optimization.

--- R/adjust_morbidity_week.R
+++ R/adjust_morbidity_week.R
@@ -4,13 +4,19 @@
 #'
 #' @param dates Either a date or a vector of dates.
 #' @param to_year The year that the years are based on.
+#' @param morbidity_weeks Optional. Morbidity week reference from tbandothers::download_morbidity_weeks. To be used to skip downloading per use.
 #'
 #' @returns A vector of morbidity weeks adjusted to provided year.
 #'
 #' @import lubridate
 #'
 #' @export
-adjust_morbidity_week <- function(dates, to_year) {
+adjust_morbidity_week <- function(dates, to_year, morbidity_weeks=NULL) {
+  # if morbidity_weeks is not given, then download
+  if (is.null(morbidity_weeks)) {
+    morbidity_weeks <- tbandothers::download_morbidity_weeks()
+  }
+
   # create vector of dates adjusted to year to_year
   adjusted_dates <- data.frame(
     day=sapply(as.Date(dates), lubridate::day),
