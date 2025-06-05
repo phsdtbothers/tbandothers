74f1fa7cd41c98d7e656d1a7f1432c14fd28c7de
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-06-05 15:15:09 +0800

    Created get_morbidity_week_shard. Properly imported tidyr.

---
+++ R/get_morbidity_week_shard.R
@@ -0,0 +1,27 @@
+#' Gets table shard for given date, based on morbidity week
+#'
+#' @param from_date date to generate shard from, if null, current date is used
+#' @param morbidity_weeks optional, morbidity week reference from tbandothers::download_morbidity_weeks()
+#'
+#' @returns string of table date shard
+#' @export
+get_date_shard <- function(from_date = Sys.Date(), morbidity_weeks = NULL) {
+  from_date <- as.Date(from_date)
+
+  # if morbidity_weeks is not given, then download
+  if (is.null(morbidity_weeks)) {
+    morbidity_weeks <- tbandothers::download_morbidity_weeks()
+  }
+
+  final_date <- NULL
+
+  mw_end <- tbandothers::get_morbidity_week_number(from_date, morbidity_weeks = morbidity_weeks) %>%
+    tbandothers::get_morbidity_week_end(morbidity_weeks = morbidity_weeks)
+
+  if (lubridate::wday(mw_end, week_start = 7) != 7) {
+    final_date <- mw_end
+  }
+  final_date <- from_date + (6 - lubridate::wday(from_date, week_start = 7))
+
+  return(final_date %>% format('%Y%m%d'))
+}
