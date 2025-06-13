4a7e5e57fc8f0e3f88bdb451728770eff702e9a0
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-06-13 12:30:49 +0800

    Improved deduplicate_ames, now retains most recent case_id. Also morbidity week download optimization.

--- R/init.R
+++ R/init.R
@@ -1,3 +1,3 @@
 #' @import utils
 # declare global variables (for no comments during devtools::check())
-utils::globalVariables(c('.', 'case_id', 'case_id_num', 'dup_completeness', 'dup_completeness_match','dup_completeness_max', 'dup_id', 'dup_points', 'dup_points_match', 'dup_points_max', 'end', 'hfmd_latest', 'proxy_onset_date', 'start', 'population', 'region', 'provcity', 'birth_date', 'words', 'words_split', 'words_soundex', 'words_soundex_in'))
+utils::globalVariables(c('.', 'case_id', 'case_id_num', 'dup_completeness', 'dup_completeness_match','dup_completeness_max', 'dup_id', 'dup_points', 'dup_points_match', 'dup_points_max', 'end', 'hfmd_latest', 'proxy_onset_date', 'start', 'population', 'region', 'provcity', 'birth_date', 'words', 'words_split', 'words_soundex', 'words_soundex_in', 'dup_case_id', 'dup_case_id_min', ''))
