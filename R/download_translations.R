2020bd4449e9fb281415d2a52484156ef4842042
ldesquitado-doh <ledesquitado@doh.gov.ph>
2025-06-25 10:19:38 +0800

    Added download_translations function. Mainly used to download and standardize name translations for data visualization generation.

---
+++ R/download_translations.R
@@ -0,0 +1,18 @@
+#' Downloads from translation reference
+#'
+#' Downloads the population reference from Google Sheet, and the given sheet name (acts as individual dictionaries for sorting).
+#'
+#' Sheet translations of system generated strings to more readable formats. Typically used for visualization generation.
+#'
+#' @param sheet_name Required, sheet name to download
+#'
+#' @returns dataframe, sourced from translation google sheet, under provided sheet_name.
+#'
+#' @import googlesheets4
+#'
+#' @export
+download_translations <- function(sheet_name) {
+  translation <- googlesheets4::read_sheet('12nyuK_Sy8WWCMOGlrwROOCB1koAX5MFFMB_iw5eaIao', sheet=sheet_name)
+
+  return(translation)
+}
